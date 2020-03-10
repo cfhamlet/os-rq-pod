3c1385a2dba60c533a0d5f6b04957e409a9decff
Ozzy <cfhamlet@gmail.com>
2020-03-10 11:55:27 +0800

    update: routers

--- app/router/router.go
+++ app/router/router.go
@@ -15,11 +15,6 @@ func InitAPIRouter(g ginserv.RouterGroup, pod *core.Pod) {
 		Path     string
 		F        controller.CtrlFunc
 	}{
-		{g.GET, "/pod/info/", controller.Info},
-		{g.POST, "/pod/pause/", controller.Pause},
-		{g.POST, "/pod/resume/", controller.Resume},
-		{g.GET, "/pod/queues/", controller.Queues},
-
 		{g.GET, "/queue/info/", controller.QueueInfo},
 		{g.GET, "/queue/view/", controller.ViewQueue},
 		{g.POST, "/queue/pause/", controller.PauseQueue},
@@ -28,12 +23,18 @@ func InitAPIRouter(g ginserv.RouterGroup, pod *core.Pod) {
 		{g.POST, "/queue/sync/", controller.SyncQueue},
 		{g.DELETE, "/queue/", controller.DeleteQueue},
 
-		{g.POST, "/request/send/", controller.AddRequest},
-		{g.GET, "/request/recv/", controller.GetRequest},
+		{g.GET, "/queues/", controller.Queues},
 
-		{g.GET, "/sys/process/memory/", controller.ProcessMemory},
-		{g.GET, "/sys/redis/memory/", controller.RedisMemory},
-		{g.GET, "/sys/redis/info/", controller.RedisInfo},
+		{g.POST, "/request/push/", controller.AddRequest},
+		{g.GET, "/request/pop/", controller.GetRequest},
+
+		{g.GET, "/sys/info/", controller.Info},
+		{g.GET, "/sys/info/process/memory/", controller.ProcessMemory},
+		{g.GET, "/sys/info/redis/memory/", controller.RedisMemory},
+		{g.GET, "/sys/info/redis/", controller.RedisInfo},
+
+		{g.POST, "/sys/ctrl/pause/", controller.Pause},
+		{g.POST, "/sys/ctrl/resume/", controller.Resume},
 	}
 
 	wp := controller.NewHandlerWrapper(pod)
