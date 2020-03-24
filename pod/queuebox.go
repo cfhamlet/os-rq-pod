a3b3564abab9c2d234cb691a5d27606912382f3f
Ozzy <cfhamlet@gmail.com>
2020-03-24 13:31:10 +0800

    add: text status

--- pod/queuebox.go
+++ pod/queuebox.go
@@ -187,8 +187,11 @@ func (box *QueueBox) SyncQueue(qid QueueID, force bool) (result Result, err erro
 		if ok {
 			result, err = queue.Sync()
 		}
+		idle := queue.Idle()
 		box.RUnlock()
-		_ = box.DeleteIdleQueue(qid)
+		if idle {
+			_ = box.DeleteIdleQueue(qid)
+		}
 	} else {
 		box.RUnlock()
 	}
@@ -271,6 +274,9 @@ func (box *QueueBox) DeleteQueue(qid QueueID) (Result, error) {
 			result, err = queue.Clear()
 			if err == nil {
 				err = queue.SetStatus(QueueRemoved)
+				if err == nil {
+					result["status"] = utils.Text(queue.Status())
+				}
 			}
 			return
 		},
@@ -394,7 +400,7 @@ func (box *QueueBox) Info() (result Result) {
 	defer box.Unlock()
 	result = Result{}
 	for k, v := range box.statusQueueIDs {
-		result[string(k)] = v.Size()
+		result[utils.Text(k)] = v.Size()
 	}
 	return
 }
